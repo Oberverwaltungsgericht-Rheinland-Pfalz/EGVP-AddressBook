0ee9e5c3ad4d36b8f012b9068aeb95c0de1123ef
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-06-18 15:31:38 +0200

    Postbox Type in der Trefferliste mit anzeigen

--- src/EgvpAdressbook/Models/EgvpAdressEntry.cs
+++ src/EgvpAdressbook/Models/EgvpAdressEntry.cs
@@ -7,40 +7,43 @@ using System.Threading.Tasks;
 
 namespace OvgRlp.Tools.EgvpAdressbook.Models
 {
-    public class EgvpAdressEntry
+  public class EgvpAdressEntry
+  {
+    public string Name { get; set; }
+    public string Organization { get; set; }
+    public string StreetAndNumber { get; set; }
+    public string PostCode { get; set; }
+    public string City { get; set; }
+    public string FederalState { get; set; }
+    public string UserId { get; set; }
+    public string Type { get; set; }
+
+    public static EgvpAdressEntry FromBusinessCardType(BusinessCardType postbox)
     {
-        public string Name { get; set; }
-        public string Organization { get; set; }
-        public string StreetAndNumber { get; set; }
-        public string PostCode { get; set; }
-        public string City { get; set; }
-        public string FederalState { get; set; }
-        public string UserId { get; set; }
+      var entry = new EgvpAdressEntry();
 
-        public static EgvpAdressEntry FromBusinessCardType(BusinessCardType postbox)
-        {
-            var entry = new EgvpAdressEntry();
+      if (null != postbox.name && !string.IsNullOrEmpty(postbox.name.Value))
+        entry.Name = postbox.name.Value;
+      if (null != postbox.organization && !string.IsNullOrEmpty(postbox.organization.Value))
+        entry.Organization = postbox.organization.Value;
+      if (null != postbox.street && !string.IsNullOrEmpty(postbox.street.Value))
+      {
+        entry.StreetAndNumber = postbox.street.Value;
+        if (null != postbox.streetNumber && !string.IsNullOrEmpty(postbox.streetNumber.Value))
+          entry.StreetAndNumber = entry.StreetAndNumber + " " + postbox.streetNumber.Value;
+      }
+      if (null != postbox.zipCode && !string.IsNullOrEmpty(postbox.zipCode.Value))
+        entry.PostCode = postbox.zipCode.Value;
+      if (null != postbox.city && !string.IsNullOrEmpty(postbox.city.Value))
+        entry.City = postbox.city.Value;
+      if (null != postbox.federalState && !string.IsNullOrEmpty(postbox.federalState.Value))
+        entry.FederalState = postbox.federalState.Value;
+      if (null != postbox.userID && !string.IsNullOrEmpty(postbox.userID.Value))
+        entry.UserId = postbox.userID.Value;
+      if (null != postbox.filterID && !string.IsNullOrEmpty(postbox.filterID.Value))
+        entry.Type = postbox.filterID.Value;
 
-            if (null != postbox.name && !string.IsNullOrEmpty(postbox.name.Value))
-                entry.Name = postbox.name.Value;
-            if (null != postbox.organization && !string.IsNullOrEmpty(postbox.organization.Value))
-                entry.Organization = postbox.organization.Value;
-            if (null != postbox.street && !string.IsNullOrEmpty(postbox.street.Value))
-            {
-                entry.StreetAndNumber = postbox.street.Value;
-                if (null != postbox.streetNumber && !string.IsNullOrEmpty(postbox.streetNumber.Value))
-                    entry.StreetAndNumber = entry.StreetAndNumber + " " + postbox.streetNumber.Value;
-            }
-            if (null != postbox.zipCode && !string.IsNullOrEmpty(postbox.zipCode.Value))
-                entry.PostCode = postbox.zipCode.Value;
-            if (null != postbox.city && !string.IsNullOrEmpty(postbox.city.Value))
-                entry.City = postbox.city.Value;
-            if (null != postbox.federalState && !string.IsNullOrEmpty(postbox.federalState.Value))
-                entry.FederalState = postbox.federalState.Value;
-            if (null != postbox.userID && !string.IsNullOrEmpty(postbox.userID.Value))
-                entry.UserId = postbox.userID.Value;
-
-            return entry;
-        }
+      return entry;
     }
+  }
 }
