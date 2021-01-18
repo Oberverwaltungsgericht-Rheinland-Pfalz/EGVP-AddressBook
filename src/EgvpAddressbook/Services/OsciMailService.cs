4f24d04d2ca622e57b6204b7f4bdaa08b0cd5ff3
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2021-01-18 07:26:58 +0100

    Neue Klasse OsciMailService implementiert
    #2

---
+++ src/EgvpAddressbook/Services/OsciMailService.cs
@@ -0,0 +1,30 @@
+﻿using OvgRlp.Tools.EgvpAddressbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvgRlp.Tools.EgvpAddressbook.Services
+{
+  public class OsciMailService
+  {
+    public const string EGVPIDPATTERN = "[EGVP-ID]";
+
+    public static void createOsciMail(string egvpAdress)
+    {
+      string to = GetOsciMail(egvpAdress);
+      System.Diagnostics.Process.Start(string.Format("mailto:{0}", to));
+    }
+
+    public static string GetOsciMail(string egvpAdress)
+    {
+      return GetOsciMailSetting().Replace(EGVPIDPATTERN, egvpAdress);
+    }
+
+    public static string GetOsciMailSetting()
+    {
+      return Properties.Settings.Default.OsciMailFormat;
+    }
+  }
+}
