a5574f7d426040e876e5d760f986b41ce0a915a9
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2021-10-05 16:58:03 +0200

    Update copyright Zeile

--- src/EgvpAddressbook/Properties/AssemblyInfo.cs
+++ src/EgvpAddressbook/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EgvpAddressbook")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 - 2021 Oberverwaltungsgericht Rheinland-Pfalz, EUPL-1.2")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
